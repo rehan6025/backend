5f3379f533364d8bba54bfc8f63f6ca9b2e1ff7f
rehan6025 <rehan6025@gmail.com>
2025-03-15 23:19:33 +0530

    completed events , async patterns and event loops (offloading tasks)

--- app.js
+++ app.js
@@ -1,6 +1,53 @@
-const _ = require('lodash')
+const EventEmitter = require('events')
 
-const arr = [1,[2 ,[3,[4]]]]
-const newArr = _.flattenDeep(arr)
+const customEmitter = new EventEmitter()
 
-console.log(newArr);
+customEmitter.on('res', ()=>{
+    console.log(`data received`);
+    
+})
+
+customEmitter.on('res', ()=>{
+    console.log(`some other logic here`);
+})
+
+customEmitter.emit('res')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const {readFile , writeFile} = require('fs').promises
+
+// const start = async () => {
+//     try {
+//         const first = await readFile('./content/first.txt', 'utf8')
+//         const second = await readFile('./content/second.txt', 'utf8')
+
+//         await writeFile('./content/result-mind-grenade.txt',
+//             `THIS IS AWESOME : ${first} ${second}`,
+//             {flag: 'a'}
+//         )
+
+//         console.log(first, second);
+        
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// start()
